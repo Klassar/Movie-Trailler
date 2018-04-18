47ec95e3499fb9944331e91d949dc313ed3639c9
Kevin Lassar <36936287+Klassar@users.noreply.github.com>
2018-04-18 16:51:04 -0400

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,105 @@
+import popcorn
+import media 
+                   
+ip_man = media.Movie("Ip Man",
+                    "93%",
+                    "https://www.rottentomatoes.com/m/ip_man",
+                    "https://upload.wikimedia.org/wikipedia/en/2/2f/Ipmanposter02.jpg",
+                    "https://www.youtube.com/watch?v=1AJxXQ7xojE",
+                     "R")
+
+avatar = media.Movie("Avatar",
+                     "83%",
+                     "https://www.rottentomatoes.com/m/avatar",
+                     "http://image.tmdb.org/t/p/original/bjVECh2nMOT2s2ThYOrmdTFVWeV.jpg",
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io",
+                     "PG-13")
+
+ice_age = media.Movie("Ice Age",
+                      "82%",
+                      "https://www.rottentomatoes.com/m/ice_age",
+                      "https://upload.wikimedia.org/wikipedia/en/3/3c/Ice_Age_%282002_film%29_poster.jpg",
+                      "https://www.youtube.com/watch?v=cMfeWyVBidk",
+                      "PG")
+
+the_kings_speech = media.Movie("The Kings Speech",
+                       "92%",
+                       "https://www.rottentomatoes.com/m/the_kings_speech",        
+                       "https://ia.media-imdb.com/images/M/MV5BMzU5MjEwMTg2Nl5BMl5BanBnXkFtZTcwNzM3MTYxNA@@._V1_SY1000_CR0,0,684,1000_AL_.jpg",
+                       "https://www.youtube.com/watch?v=kYoSQkfrjfA",
+                       "PG-13")
+
+the_dark_knight = media.Movie("The Dark Knight",
+                       "94%",
+                       "https://www.rottentomatoes.com/m/the_dark_knight",       
+                       "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+                       "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+                       "PG-13")
+
+harry_potter = media.Movie("Harry Potter And The Sorcerer's stone",
+                       "82%",
+                       "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone",    
+                       "https://vignette.wikia.nocookie.net/harrypotter/images/0/0e/Philostone.jpg/revision/latest/scale-to-width-down/1000?cb=20180318153750",
+                       "https://www.youtube.com/watch?v=VyHV0BRtdxo",
+                       "PG")
+
+big_fish = media.Movie("Big Fish",
+                       "89%",
+                       "https://www.rottentomatoes.com/m/1127787_big_fish?",
+                       "https://upload.wikimedia.org/wikipedia/en/4/41/Big_Fish_movie_poster.png",
+                       "https://www.youtube.com/watch?v=M3YVTgTl-F0",
+                       "PG-13")
+
+rush_hour = media.Movie("Rush Hour",
+                       "78%",
+                       "https://www.rottentomatoes.com/m/rush_hour",
+                       "https://ia.media-imdb.com/images/M/MV5BYWM2NDZmYmYtNzlmZC00M2MyLWJmOGUtMjhiYmQ2OGU1YTE1L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,678,1000_AL_.jpg",
+                       "https://www.youtube.com/watch?v=JMiFsFQcFLE",
+                       "PG-13")
+
+wall_e = media.Movie("WALL-E",
+                       "89%",
+                       "https://www.rottentomatoes.com/m/wall_e",
+                       "https://ia.media-imdb.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                       "https://www.youtube.com/watch?v=8-_9n5DtKOc",
+                       "G")
+
+three_hundred = media.Movie("300",
+                       "89%",
+                       "https://www.rottentomatoes.com/m/300",
+                       "https://ia.media-imdb.com/images/M/MV5BMjAzNTkzNjcxNl5BMl5BanBnXkFtZTYwNDA4NjE3._V1_.jpg",
+                       "https://www.youtube.com/watch?v=UrIbxk7idYA",
+                       "R")
+
+mean_girls = media.Movie("Mean Girls",
+                       "66%",
+                       "https://www.rottentomatoes.com/m/mean_girls",
+                       "https://ia.media-imdb.com/images/M/MV5BMjE1MDQ4MjI1OV5BMl5BanBnXkFtZTcwNzcwODAzMw@@._V1_SY1000_CR0,0,706,1000_AL_.jpg",
+                       "https://www.youtube.com/watch?v=KAOmTMCtGkI",
+                       "PG-13")
+
+slumdog_millionaire = media.Movie("Slumdog Millionaire",
+                       "90%",
+                       "https://www.rottentomatoes.com/m/slumdog_millionaire",
+                       "https://ia.media-imdb.com/images/M/MV5BZmNjZWI3NzktYWI1Mi00OTAyLWJkNTYtMzUwYTFlZDA0Y2UwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                       "https://www.youtube.com/watch?v=AIzbwV7on6Q",
+                       "R")
+
+#Define instance name of every movie to be visible on website
+movies = [
+    ip_man,
+    avatar,
+    harry_potter,
+    ice_age,    
+    the_kings_speech,
+    the_dark_knight,
+    big_fish,
+    rush_hour,
+    wall_e,
+    three_hundred,
+    mean_girls,
+    slumdog_millionaire    
+]
+
+#Opens popcorn file to dispaly movie trailer page
+popcorn.open_movies_page(movies)
---
+++ popcorn.py
@@ -0,0 +1,293 @@
+import webbrowser
+import os
+import re
+
+
+# Styles and scripting for the page
+main_page_head = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Popcorn Trailers</title>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+    <style type="text/css" media="screen">
+
+        /************  Base Styles ************/ 
+        html {
+            padding: 0;
+            margin: 0;
+        }       
+        body {
+            padding: 3em 0 0.5em 0;
+            margin: inherit;
+            background-image: url("https://www-cdn01.tivo.com/sites/all/themes/tivo3/images/shows_two_discover.jpg");
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center; 
+            background-size: cover;
+        }
+        h1,h2,h3,h4,h5,h6,p {
+            padding: 0;
+            margin: 0;
+        }
+        a,
+        a:hover,
+        a:focus {
+            color: #fff;
+        }        
+        .movieTrailers-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        @media(min-width: 400px) {
+            .movieTrailers-container {
+                flex-flow: row wrap;
+                justify-content: space-around;
+                width: 90%;
+                max-width: 1200px;
+                margin: 0 auto;                
+            }        
+        }        
+        .movie-tile {
+            margin-bottom: 50px;
+            padding-top: 50px;
+            padding: 0 1em;
+            transition: transform .1s ease-out;
+            color: #fff;
+        }
+        .movie-tile:hover {
+            transform: scale(1.1);
+            cursor: pointer;
+        }
+        .detials-wrapper {
+            padding-top: 1em;
+        }
+        .rating-container {
+            width: 50%;
+            float: left;
+            transition: all .1s ease-out;
+        }
+        .rating-icon {
+            width: 35px;
+            height: 35px;
+            margin-bottom: -7px;
+        }
+        .rating {
+            display: inline;
+            font-size: 23px;
+        }
+        .info-container {
+            width: 50%;
+            float: right;
+            background: #f93109;
+            margin-top: 4px;
+            padding: 5px 0;
+            border-radius: 100px;
+            transition: all .1s ease-out;
+        }
+        .info {
+            text-align: center;
+        }
+        .rating-container:active {
+            transform: scale(0.8);
+        }
+
+        .container {
+            display: none;
+        }
+
+        .modal {
+            justify-content: center;
+            align-content: center;
+            align-self: center;
+            align-items: center;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;          
+        }
+
+        #trailer .modal-dialog {
+            width: 85vw;
+            height: auto;
+        }
+        .hanging-close {
+            position: absolute;
+            top: 70px;
+            left: 390px
+            z-index: 9001;
+
+            display: none;
+        }
+        #trailer-video {
+            width: 100%;
+            height: 100%;
+        }
+        .scale-media {
+            padding-bottom: 56.25%;
+            position: relative;
+        }
+        .scale-media iframe {
+            border: none;
+            height: 100%;
+            position: absolute;
+            width: 100%;
+            left: 0;
+            top: 0;
+            background-color: white;
+
+
+        }
+
+        .show {
+            display: flex !important;
+        }
+
+        .hide {
+            display: none !important;
+        }
+
+
+        .showModal {
+            position: fixed;
+            display: flex;
+            background: rgba(0, 0, 0, 0.6)
+        }
+
+        .hideModal {
+            position: none;
+            display: none;
+        }
+
+        
+    </style>
+    <script type="text/javascript" charset="utf-8">
+
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+            // Remove the src so the player itself gets removed, as this is the only
+            // reliable way to ensure the video stops playing in IE
+            $("#trailer-video-container").empty();
+            $(".modal").removeClass("showModal");
+            $(".modal").addClass("hideModal");
+        });
+
+        $(document).on('click', '.movie-tile', function (event) {
+            $(".modal").removeClass("hideModal");
+            $(".modal").addClass("showModal");
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+              'id': 'trailer-video',
+              'type': 'text-html',
+              'src': sourceUrl,
+              'frameborder': 0
+            }));
+        });       
+
+        $(document).ready(function () {
+          $('.movie-tile').hide().first().show("fast", function showNext() {
+            $(this).next("div").fadeIn("fast", showNext);
+          });
+        });        
+
+        $(document).ready(function () {
+            $(".info-modal").addClass("hide");
+        }); 
+        
+    </script>
+</head>
+'''
+
+# The main page layout and title bar
+main_page_content = '''
+  <body>
+    <!-- Main Page Content -->
+    <div class="container">
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="movieTrailers-container">
+      {movie_tiles}
+    </div>
+
+    <!-- Trailer Video Modal -->
+    <div class="modal" id="trailer">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
+          </a>
+          <div class="scale-media" id="trailer-video-container">
+          </div>
+        </div>
+      </div>
+    </div>   
+
+  </body>
+</html>
+'''
+
+# A single movie entry html template
+movie_tile_content = '''
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img id="posterImage" src="{poster_image_url}" width="220" height="342">
+    <div class="detials-wrapper">
+        <a id="rotten-tomatoes-link" href="{reviews_link}" target="_blank"><div class="rating-container">
+            <img class="rating-icon" src="https://staticv2-4.rottentomatoes.com/static/images/icons/new-upright.png">
+            <h3 class="rating">{audience_rating}</h3>
+        </div></a>
+        <div id="ic" class="info-container">
+            <h3 class="info">{age_restriction}</h3>          
+        </div>
+    </div>
+</div>
+'''
+
+def create_movie_tiles_content(movies):
+    # The HTML content for this section of the page
+    content = ''
+    
+    for movie in movies:
+        # Extract the youtube ID from the url
+        youtube_id_match = re.search(
+            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(
+            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                              else None)
+
+        # Append the tile for the movie with its content filled in
+        content += movie_tile_content.format(
+            movie_title=movie.title,
+            audience_rating=movie.rating,
+            reviews_link=movie.link,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id,                      
+            age_restriction=movie.restriction
+        )        
+    return content
+
+def open_movies_page(movies):
+    # Create or overwrite the output file
+    output_file = open('popcorn.html', 'w')
+
+    # Replace the movie tiles placeholder generated content
+    rendered_content = main_page_content.format(
+        movie_tiles=create_movie_tiles_content(movies))
+
+    # Output the file
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
+
+    # open the output file in the browser (in a new tab, if possible)
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://' + url, new=2)
